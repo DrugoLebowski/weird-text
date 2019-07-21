e32fc430f8eaee5d3d208283646237bbc7a80504
Valerio Belli <belli.valerio@gmail.com>
2019-07-21 10:05:30 +0200

    Update DecoderText

--- src/components/DecoderText.jsx
+++ src/components/DecoderText.jsx
@@ -2,48 +2,51 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
-// Internal
-import { simpleDecoder } from '../utils/decoder';
+const Style = styled.div`
+  display: flex;
+  align-items: flex-start;
+`;
 
 const DecoderText = (props) => {
   const [textToDecode, setTextToDecode, ] = useState('');
+  const [words, setWords, ] = useState('');
   const [originalWords, setOriginalWords ] = useState([]);
   const [decodedText, setDecodedText, ] = useState('');
 
   const onOriginalWordsChange = (e) => {
-    const wordsList = e.target.value.split(' ');
-
-    console.log(textToDecode, props.encodedText);
-
-    if (textToDecode !== props.ncodedText) {
+    if (textToDecode !== props.encodedText) {
       props.setErrorMessage('There is a mismatch among encoded text e the text to decode!');
     } else {
       props.setErrorMessage(null);
-      setOriginalWords(wordsList);
-      setDecodedText(simpleDecoder(textToDecode, originalWords));
+      setOriginalWords(e.target.value.split(' '));
+      setDecodedText(props.decoder(textToDecode, originalWords));
     }
   }
 
   return (
-    <>
+    <Style>
       <h2>Decoder</h2>
-        <h3>Input</h3>
-        <h4>Text to decode</h4>
-        <textarea
-          rows="5"
-          value={textToDecode}
-          onChange={e => setTextToDecode(e.target.value)}/>
+      <h3>Input</h3>
+      <h4>Text to decode</h4>
+      <textarea
+        rows="5"
+        value={textToDecode}
+        onChange={e => setTextToDecode(e.target.value)}/>
 
-        <h4>List of the original words that got encoded, space separated.</h4>
-        <div>
-          <input type="text" onChange={onOriginalWordsChange}/>
-        </div>
+      <h4>List of the original words that got encoded, space separated.</h4>
+      <div>
+        <input type="text"
+          value={words}
+          onChange={onOriginalWordsChange}/>
+      </div>
 
-        <h3>Output</h3>
-        <h4>Decoded text</h4>
-        <div>Here the text.</div>
-        <div>{decodedText}</div>
-    </>
+      <h3>Output</h3>
+      <h4>Decoded text</h4>
+      <div>Here the text.</div>
+      <div id='decoded-text'>
+        {decodedText}
+      </div>
+    </Style>
   );
 }
 
