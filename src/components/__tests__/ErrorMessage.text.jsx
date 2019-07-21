8c5149ca5e823ca875fd4cae2d337a02cdc5095a
Valerio Belli <belli.valerio@gmail.com>
2019-07-21 10:27:45 +0200

    Add ErrorMessage tests

---
+++ src/components/__tests__/ErrorMessage.text.jsx
@@ -0,0 +1,53 @@
+// Vendor
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { act } from 'react-dom/test-utils';
+
+// Internal
+import ErrorMessage from '../ErrorMessage';
+
+describe('ErrorMessage', () => {
+  let container = null;
+
+  beforeEach(() => {
+    container = document.createElement('container');
+    document.body.appendChild(container);
+  });
+
+  it('should not show message', () => {
+    // Arrange
+    const message = 'Hello world';
+
+    act(() => {
+      ReactDOM.render(
+        <ErrorMessage
+          message={message} />,
+        container
+      );
+    });
+
+    // Assert
+    expect(document.querySelector('div').textContent).toEqual(message);
+  });
+
+  it('should show message', () => {
+    // Arrange
+    const message = null;
+
+    act(() => {
+      ReactDOM.render(
+        <ErrorMessage
+          message={message} />,
+        container
+      );
+    });
+
+    // Assert
+    expect(document.querySelector('div').textContent).toEqual('');
+  });
+
+  afterEach(() => {
+    document.body.removeChild(container);
+    container = null;
+  });
+});
